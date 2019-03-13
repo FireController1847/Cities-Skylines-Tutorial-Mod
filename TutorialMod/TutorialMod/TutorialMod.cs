1b32ed17f0a55223634c7bc2cd70fd4ef45ac480
FireController#1847 <usfirepilot123@gmail.com>
2019-03-12 20:25:15 -0600

    Part 1

---
+++ TutorialMod/TutorialMod/TutorialMod.cs
@@ -0,0 +1,16 @@
+﻿using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialMod
+{
+    public class TutorialMod : IUserMod
+    {
+        public string Name => "Tutorial Mod";
+        public string Description => "This is a mod that is a tutorial!";
+    }
+}
